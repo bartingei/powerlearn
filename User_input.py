ecf601c0b99c25d19dbe6d499869d17d2c01d8a1
johnpaul kibet <johnpaulkibet3@gmail.com>
2024-11-07 11:47:03 +0300

    intro to py

---
+++ User_input.py
@@ -0,0 +1,5 @@
+name = input("Hello enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
